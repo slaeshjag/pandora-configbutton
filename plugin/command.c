556562536741454624afa62acc9e8ce265f9d122
Steven Arnow <s@rdw.se>
2014-05-01 16:43:16 +0200

    More work on command execute plugin

--- plugin/command.c
+++ plugin/command.c
@@ -7,8 +7,11 @@
 const char plugin_name[] = "Execute command";
 const char plugin_desc[] = "Adds a menu of user-defined shell commands";
 
+#define	PRIMARY_ICON	"/usr/share/icons/pandora/exec_command.png";
+
 struct entry {
 	char		*command;
+	char		*icon;
 	char		*name;
 };
 
@@ -24,25 +27,63 @@ int activate(void *internal) {
 }
 
 
-int getinfo(PLUGIN_INFO *info) {
-	struct internal *i;
-	int i;
-	char path[PATH_MAX];
-	FILE *fp;
+void pluginExecute(char *command) {
+	system(command);
+	return;
+}
 
-	i = info->internal;
+
+int getinfo(PLUGIN_INFO *info) {
+	struct internal *in;
+	int i;
+	char path[PATH_MAX], buff_name[64], buff_icon[PATH_MAX], buff_command[2048], *r;
+	FILE *fp;
+	struct PLUGIN_SUBMENU *sub;
+
+	in = info->internal;
 	if (!info->internal) {
 		sprintf(path, "%s/.config-button/commands.presets", getenv("HOME"));
 		if (!(fp = fopen(path, "r"))) {
 			fprintf(stderr, "Unable to load commands\n");
-			return -1;
+			goto build_menu;
 		}
 		
 
-		/* Figure out how many commands there are */
-		
-		i = malloc(sizeof(*i));
-		/* Init */
+		in = malloc(sizeof(*in));
+		in->entry = NULL;
+		in->entries = 0;
+
+		for (i = 0; !foef(fp); i++) {
+			*buff_name = *buff_icon = *buff_command = 0;
+			fscanf(fp, "%[^\t\n] %[^\t\n] %[^\t\n]\n", buff_name, buff_icon, buff_command);
+			if (!(*buff_name))
+				break;
+			in->entry = realloc(in->entry, (i + 1) * sizeof(*in->entry));
+			in->entries = i + 1;
+			in->entry[i].command = strcpy(buff_command);
+			if (*buff_icon)
+				in->entry[i].icon = strcpy(buff_icon);
+			else
+				in->entry[i].icon = strcpy(PRIMARY_ICON);
+			in->entry[i].name = strcpy(buff_name);
+		}
+
+		info->internal = in;
+
+		info->label = "Execute command";
+		info->icon_path = PRIMARY_ICON;
+		info->sort_hint = 5;
+
+		for (i = in->entries - 1; i >= 0; i--) {
+			sub = info->submenu;
+			info->submenu = malloc(sizeof(*info->submenu));
+			info->submenu->next = sub;
+			info->submenu->label = in->entry[i].name;
+			info->submenu->internal = in->entry[i].command;
+			info->submenu->icon_path = in->entry[i].icon;
+			info->submenu->visible = 1;
+			info->submenu->activate = pluginExecute;
+		}
 	}
 
 	return 0;

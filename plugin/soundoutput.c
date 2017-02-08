8cf1862339f6347ef75ee695e53afafcde1af1ee
Alexander Matthes <ziz@mailbox.org>
2017-02-08 16:15:50 +0100

    Added more options and easy adding of new devices to the sound output plugin

--- plugin/soundoutput.c
+++ plugin/soundoutput.c
@@ -11,7 +11,7 @@
 #include "pas.h" //VERSION 0.4 as base64
 
 const char plugin_name[] = "Sound Output";
-const char plugin_desc[] = "Adds a menu for connecting to bluetooth head sets";
+const char plugin_desc[] = "Adds a menu for connecting to bluetooth headsets";
 static char pas_path[520];
 static char recent_device[64] = "unknown";
 
@@ -31,18 +31,19 @@ typedef struct sDevice *pDevice;
 typedef struct sDevice
 {
 	char name[64];
+	char mac[17];
 	pDevice next;
 } tDevice;
 
 static pDevice first_device = NULL;
 
-static void cleanDevices()
+static void cleanDevices(pDevice *first)
 {
-	while (first_device)
+	while (*first)
 	{
-		pDevice next = first_device->next;
-		free(first_device);
-		first_device = next;
+		pDevice next = (*first)->next;
+		free(*first);
+		*first = next;
 	}
 }
 
@@ -52,10 +53,11 @@ int prefix(const char *pre, const char *str)
 }
 
 #define PREFIX ".asoundrc_"
+
 static void updateDevices(int recent_found)
 {
 	char cmd[1024];
-	cleanDevices();
+	cleanDevices(&first_device);
 	DIR* d = opendir(pas_path);
 	if (d)
 	{
@@ -90,7 +92,44 @@ static void updateDevices(int recent_found)
 		sprintf(recent_device,"unknown");
 }
 
+pDevice getBluetoothDevices()
+{
+	pDevice first = NULL;
+	char filename[1024];
+	DIR* d = opendir("/var/lib/bluetooth");
+	if (d)
+	{
+		struct dirent *dir;
+		while ((dir = readdir(d)) != NULL)
+		{
+			if (strcmp(dir->d_name,".") == 0 || strcmp(dir->d_name,"..") == 0)
+				continue;
+			sprintf(filename,"/var/lib/bluetooth/%s/names",dir->d_name);
+			FILE* fp = fopen(filename, "r");
+			if (fp)
+			{
+				pDevice dev = (pDevice)malloc(sizeof(tDevice));
+				while (fscanf(fp, "%s %[^\n]", dev->mac, dev->name) > 1)
+				{
+					dev->next = first;
+					first = dev;
+					dev = (pDevice)malloc(sizeof(tDevice));
+				}
+				free(dev);
+				fclose(fp);
+			}
+		}
+		closedir(d);
+	}
+	return first;
+}
+
 GtkWidget *win;
+GtkListStore *model;
+GtkWidget *lbox;
+GtkWidget *add_name, *add_mac;
+GtkWidget *win_edit;
+GtkTextBuffer *tb;
 
 static void extract_pas(const char* path)
 {
@@ -111,10 +150,10 @@ static void extract_pas(const char* path)
 	sprintf(buffer,"rm -f %s/pas_temp.zip",path);
 	system(buffer);
 	//replace PAS_PATH=/home/${USER}/pas/ with choosen path
-	sprintf(buffer,"sed -ie 's,PAS_PATH=/home/${USER}/pas,PAS_PATH=%s,' %s/pas.sh",path,path);
+	sprintf(buffer,"sed -i 's,PAS_PATH=/home/${USER}/pas,PAS_PATH=%s,' %s/pas.sh",path,path);
 	system(buffer);
 	
-	updateDevices(1);
+	updateDevices(0);
 }
 
 static void messageBox( char* message )
@@ -147,17 +186,299 @@ static void applyPath(GtkWidget *b, gpointer *e)
 		extract_pas(pas_path);
 		messageBox( "Created \"pas.sh\" at location" );
 	}
-
+	gtk_widget_destroy(win);
 }
 
-static void closeWindow(GtkWidget *w, gpointer null)
+static pDevice getDeviceAndNr(GtkTreeView* l, int* id,pDevice* prev)
 {
-	gtk_widget_destroy(win);
+	GList* list;
+	GtkTreeSelection *s = gtk_tree_view_get_selection( GTK_TREE_VIEW( l ) );
+	if (gtk_tree_selection_count_selected_rows( s ) <= 0 )
+		return NULL;	
+	list = gtk_tree_selection_get_selected_rows( s, NULL );
+	GtkTreePath* path = g_list_first(list)->data;
+	*id = gtk_tree_path_get_indices( path )[0];
+	pDevice dev = first_device;
+	if (prev)
+		*prev = NULL;
+	int i = 0;
+	while (dev)
+	{
+		if (i == *id)
+			break;
+		i++;
+		if (prev)
+			*prev = dev;
+		dev = dev->next;
+	}
+	g_list_foreach( list, (GFunc) gtk_tree_path_free, NULL );
+	g_list_free( list );
+	return dev;
+}
+
+static void removeDevice(GtkWidget *b, gpointer *l)
+{
+	GtkWidget *d;
+	GtkTreeIter iter;
+	char filename[1024];
+	int id;
+	pDevice prev, dev = getDeviceAndNr((GtkTreeView*)l, &id, &prev);
+	if (dev)
+	{
+		char buffer[256];
+		sprintf(buffer,"Do you really want to remove \"%s\"? This cannot be undone!",dev->name);
+		d = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_WARNING, GTK_BUTTONS_YES_NO, buffer);
+		if (gtk_dialog_run(GTK_DIALOG(d)) == GTK_RESPONSE_YES)
+		{
+			if (gtk_tree_model_iter_nth_child(GTK_TREE_MODEL(model),&iter,NULL,id))
+				gtk_list_store_remove(model,&iter);
+			if (prev)
+				prev->next = dev->next;
+			else
+				first_device = dev->next;
+			sprintf(filename,"%s/"PREFIX"%s",pas_path,dev->name);
+			remove(filename);
+			free(dev);
+		}
+		gtk_widget_destroy(d);
+	}
+}
+
+pDevice findDevice(pDevice first,const char* name)
+{
+	pDevice dev = first;
+	while (dev)
+	{
+		if (strcmp(name,dev->name) == 0)
+			break;
+		dev = dev->next;
+	}
+	return dev;
+}
+
+
+static void addDevice(GtkWidget *b, gpointer *__window)
+{
+	GtkWidget *d;
+	GtkWidget *window = GTK_WIDGET(__window);
+	const gchar* name = gtk_entry_get_text(GTK_ENTRY(add_name));
+	char buffer[256] = "";
+	if (findDevice(first_device,name))
+		sprintf(buffer,"Device name \"%s\" already in use!",name);
+	if (strcmp(name,"bt") == 0)
+		sprintf(buffer,"Device name \"%s\" not alowed (would overwrite template file)!",name);
+	if (strchr(name,' '))
+		sprintf(buffer,"Device name \"%s\" not alowed because of spaces!",name);
+	if (strchr(name,'\"'))
+		sprintf(buffer,"Device name \"%s\" not alowed because of \"!",name);
+	if (buffer[0])
+	{
+		d = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, buffer);
+		gtk_dialog_run(GTK_DIALOG(d));
+		gtk_widget_destroy(d);
+		return;
+	}
+	const gchar* mac = gtk_entry_get_text(GTK_ENTRY(add_mac));
+	char cmd[1024];
+	sprintf(cmd, "sed 's/device \"XX:XX:XX:XX:XX:XX\"/device \"%s\"/'  %s/.asoundrc_bt > %s/.asoundrc_%s", mac, pas_path, pas_path, name );
+	system(cmd);
+	updateDevices(1);
+	gtk_widget_destroy(GTK_WIDGET(window));
+}
+
+static void saveText(GtkWidget *b, gpointer *name)
+{
+	GtkTextIter start, end;
+	char buffer[1024];
+	gtk_text_buffer_get_start_iter( tb, &start );
+	gtk_text_buffer_get_end_iter( tb , &end );
+	gchar* file_buffer = gtk_text_buffer_get_text( tb , &start, &end, FALSE );
+	sprintf( buffer, "%s/.asoundrc_%s",pas_path,(char*)name);
+	g_file_set_contents (buffer,file_buffer,-1, NULL);
+	g_free(file_buffer);
+	gtk_widget_destroy(GTK_WIDGET(win_edit));
+}
+
+static void closeWindow(GtkWidget *w, gpointer window)
+{
+	gtk_widget_destroy(GTK_WIDGET(window));
+}
+
+gboolean view_selection_func(
+	GtkTreeSelection *selection,
+	GtkTreeModel     *model,
+	GtkTreePath      *path,
+	gboolean          path_currently_selected,
+	gpointer          null)
+{
+	GtkTreeIter iter;
+	if (!path_currently_selected && gtk_tree_model_get_iter(model, &iter, path))
+	{
+		gchar *mac;
+		gchar *name;
+		gtk_tree_model_get(model, &iter, 0, &mac, -1);
+		gtk_tree_model_get(model, &iter, 1, &name, -1);
+		int i;
+		for (i=0;name[i];++i)
+			if (name[i] == ' ' || name[i] == '\"')
+				name[i] = '_';
+		gtk_entry_set_text(GTK_ENTRY(add_mac), mac );
+		gtk_entry_set_text(GTK_ENTRY(add_name), name );
+		g_free(mac);
+		g_free(name);
+	}
+	return TRUE;
+}
+
+static void addDeviceDialog(GtkWidget *w, gpointer null)
+{
+	GtkWidget *win_add, *vbox, *hbox, *b, *sbox;
+	GtkCellRenderer *renderer;
+	GtkTreeIter iter;
+	pDevice first_bt = getBluetoothDevices();
+	win_add = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_position(GTK_WINDOW(win_add), GTK_WIN_POS_CENTER);
+	gtk_window_set_title(GTK_WINDOW(win_add), "Add new (bluetooth) device");
+	gtk_window_set_modal(GTK_WINDOW(win_add),TRUE);
+	gtk_window_set_default_size(GTK_WINDOW(win_add),-1,300);
+
+	vbox = gtk_vbox_new(FALSE, 0);
+	gtk_container_add(GTK_CONTAINER(win_add), vbox);
+	b = gtk_label_new("Enter the MAC adress of the bluetooth device and a name either by hand or from the list below. You can later further edit the .asoundrc_ file in the sound output plugin settings if needed.");
+	gtk_label_set_line_wrap(GTK_LABEL(b), TRUE);
+	gtk_box_pack_start(GTK_BOX(vbox), b, FALSE, FALSE, 5);
+
+	hbox = gtk_hbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 5);
+	add_mac = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(hbox), add_mac, FALSE, FALSE, 5);
+	gtk_entry_set_text(GTK_ENTRY(add_mac), "XX:XX:XX:XX:XX:XX" );
+	gtk_entry_set_max_length(GTK_ENTRY(add_mac), 17 );
+	add_name = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(hbox), add_name, FALSE, FALSE, 5);
+	gtk_entry_set_text(GTK_ENTRY(add_name), "name" );
+	gtk_entry_set_max_length(GTK_ENTRY(add_mac), 64 );
+	b = gtk_button_new_from_stock(GTK_STOCK_ADD);
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(addDevice), win_add);
+	gtk_box_pack_start(GTK_BOX(hbox), b, FALSE, FALSE, 5);
+	b = gtk_button_new_from_stock(GTK_STOCK_CLOSE);
+	gtk_box_pack_start(GTK_BOX(hbox), b, FALSE, FALSE, 5);
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(closeWindow), win_add);
+
+	sbox = gtk_scrolled_window_new(NULL, NULL);
+	gtk_box_pack_start(GTK_BOX(vbox), sbox, TRUE, TRUE, 5);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sbox), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC );
+
+	hbox = gtk_tree_view_new();
+	gtk_container_add(GTK_CONTAINER(sbox), hbox);
+	renderer = gtk_cell_renderer_text_new();
+	gtk_tree_view_insert_column_with_attributes(
+		GTK_TREE_VIEW(hbox),
+		-1,
+		"MAC",
+		renderer,
+		"text",
+		0,
+		NULL);
+	gtk_tree_view_insert_column_with_attributes(
+		GTK_TREE_VIEW(hbox),
+		-1,
+		"Name",
+		renderer,
+		"text",
+		1,
+		NULL);
+	model = gtk_list_store_new( 2, G_TYPE_STRING, G_TYPE_STRING );
+	pDevice bt_dev = first_bt;
+	while (bt_dev)
+	{
+		gtk_list_store_append( model, &iter );
+		gtk_list_store_set( model, &iter, 0, bt_dev->mac, 1, bt_dev->name, -1 );
+		bt_dev = bt_dev->next;
+	}
+	cleanDevices(&first_bt);
+	gtk_tree_view_set_model( GTK_TREE_VIEW( hbox ), GTK_TREE_MODEL( model ) );
+	gtk_tree_selection_set_select_function(gtk_tree_view_get_selection(GTK_TREE_VIEW(hbox)), view_selection_func, NULL, NULL);	
+	gtk_widget_show_all(win_add);
+}
+
+static void editDeviceDialog(GtkWidget *w, gpointer l)
+{
+	GtkWidget *vbox, *hbox, *b, *sbox;
+	char buffer[1024];
+	int id;
+	gchar *file_buffer;
+	pDevice prev, dev = getDeviceAndNr((GtkTreeView*)l, &id, &prev);
+	if (!dev)
+		return;
+	win_edit = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_position(GTK_WINDOW(win_edit), GTK_WIN_POS_CENTER);
+	sprintf(buffer, "Edit audio device \"%s\"",dev->name);
+	gtk_window_set_title(GTK_WINDOW(win_edit), buffer);
+	gtk_window_set_modal(GTK_WINDOW(win_edit),TRUE);
+	gtk_window_set_default_size(GTK_WINDOW(win_edit),600,420);
+
+	vbox = gtk_vbox_new(FALSE, 0);
+	gtk_container_add(GTK_CONTAINER(win_edit), vbox);
+	b = gtk_label_new("Edit the asoundrc_ file for your needs. Keep in mind, there is now correctness check, so you may make the config file invalid.");
+	gtk_label_set_line_wrap(GTK_LABEL(b), TRUE);
+	gtk_widget_set_size_request( b, 600, -1);
+	gtk_box_pack_start(GTK_BOX(vbox), b, FALSE, FALSE, 5);
+
+	sbox = gtk_scrolled_window_new(NULL, NULL);
+	gtk_box_pack_start(GTK_BOX(vbox), sbox, TRUE, TRUE, 5);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sbox), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC );
+	b = gtk_text_view_new();
+	gtk_container_add(GTK_CONTAINER(sbox), b);
+	gtk_widget_modify_font( b, pango_font_description_from_string("MONOSPACE"));
+	tb = gtk_text_view_get_buffer( GTK_TEXT_VIEW(b));
+	sprintf( buffer, "%s/.asoundrc_%s",pas_path,dev->name);
+	if (g_file_get_contents (buffer,&file_buffer,NULL, NULL))
+	{
+		gtk_text_buffer_set_text( tb, file_buffer, -1 );
+		g_free(file_buffer);
+	}
+	else
+		gtk_text_buffer_set_text( tb, "ERROR: Could not load config!", -1 );
+
+	hbox = gtk_hbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 5);
+	b = gtk_label_new("");
+	gtk_box_pack_start(GTK_BOX(hbox), b, TRUE, TRUE, 5);
+	b = gtk_button_new_with_mnemonic("_Discard");
+	gtk_button_set_image(GTK_BUTTON(b), gtk_image_new_from_stock( GTK_STOCK_CANCEL, GTK_ICON_SIZE_BUTTON ) );
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(closeWindow), win_edit);
+	gtk_box_pack_start(GTK_BOX(hbox), b, FALSE, FALSE, 5);
+	b = gtk_button_new_from_stock(GTK_STOCK_SAVE);
+	gtk_box_pack_start(GTK_BOX(hbox), b, FALSE, FALSE, 5);
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(saveText), dev->name);
+
+	gtk_widget_show_all(win_edit);
+}
+
+void set_pas_path()
+{
+	char path[1024];
+	FILE *fp;
+
+	sprintf(path, "%s/.config-button/sound-output.conf", getenv("HOME"));
+	fp = fopen(path, "r");
+	if (fp)
+	{
+		fscanf(fp, "%s\n", pas_path);
+		fclose(fp);
+	}
+	else
+		sprintf(pas_path, "%s/pas", getenv("HOME"));
 }
 
 void configure()
 {
-	GtkWidget *vbox, *hbox, *b, *e;
+	set_pas_path();
+	GtkWidget *vbox, *hbox, *b, *e, *subvbox, *sbox;
+	GtkCellRenderer *renderer;
+	GtkTreeIter iter;
+
 	updateDevices(1);
 	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_position(GTK_WINDOW(win), GTK_WIN_POS_CENTER);
@@ -166,26 +487,60 @@ void configure()
 
 	vbox = gtk_vbox_new(FALSE, 0);
 	gtk_container_add(GTK_CONTAINER(win), vbox);
-	b = gtk_label_new("Path to \"pas.sh\". If it is not found, version 0.4 will installed automaticly:");
+	b = gtk_label_new("Path to \"pas.sh\". If the folder does not contain \"pas.sh\", version 0.4 will be installed in it.");
 	gtk_box_pack_start(GTK_BOX(vbox), b, FALSE, FALSE, 5);
-
+	
 	hbox = gtk_hbox_new(FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 5);
 	e = gtk_entry_new();
 	gtk_box_pack_start(GTK_BOX(hbox), e, FALSE, FALSE, 5);
 	gtk_entry_set_text(GTK_ENTRY(e), pas_path );
 	gtk_entry_set_max_length(GTK_ENTRY(e), 520 );
+	gtk_entry_set_width_chars(GTK_ENTRY(e), 44 );
 	b = gtk_button_new_from_stock(GTK_STOCK_APPLY);
 	gtk_box_pack_start(GTK_BOX(hbox), b, FALSE, FALSE, 5);
-	gtk_entry_set_width_chars(GTK_ENTRY(e), 44 );
 	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(applyPath), e);
 
 	hbox = gtk_hbox_new(FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 5);
+	sbox = gtk_scrolled_window_new(NULL, NULL);
+	gtk_box_pack_start(GTK_BOX(hbox), sbox, TRUE, TRUE, 5);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sbox), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC );
+	lbox = gtk_tree_view_new();
+	gtk_container_add(GTK_CONTAINER(sbox), lbox);
+	renderer = gtk_cell_renderer_text_new();
+	gtk_tree_view_insert_column_with_attributes(
+		GTK_TREE_VIEW(lbox),
+		-1,
+		"Sound output device name",
+		renderer,
+		"text",
+		0,
+		NULL);
+	model = gtk_list_store_new( 1, G_TYPE_STRING );
+	pDevice dev = first_device;
+	while (dev)
+	{
+		gtk_list_store_append( model, &iter );
+		gtk_list_store_set( model, &iter, 0, dev->name, -1 );
+		dev = dev->next;
+	}
+	gtk_tree_view_set_model( GTK_TREE_VIEW( lbox ), GTK_TREE_MODEL( model ) );
+	//buttons on the right	
+	subvbox = gtk_vbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), subvbox, FALSE, FALSE, 5);
+	b = gtk_button_new_with_mnemonic("_Edit directly");
+	gtk_button_set_image(GTK_BUTTON(b), gtk_image_new_from_stock( GTK_STOCK_EDIT, GTK_ICON_SIZE_BUTTON ) );
+	gtk_box_pack_start(GTK_BOX(subvbox), b, FALSE, FALSE, 5);
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(editDeviceDialog), lbox );
+	b = gtk_button_new_from_stock(GTK_STOCK_REMOVE);
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(removeDevice), lbox );
+	gtk_box_pack_start(GTK_BOX(subvbox), b, FALSE, FALSE, 5);
 	b = gtk_button_new_from_stock(GTK_STOCK_CLOSE);
-	gtk_box_pack_start(GTK_BOX(hbox), b, FALSE, FALSE, 5);
-	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(closeWindow), NULL);
-
+	gtk_box_pack_start(GTK_BOX(subvbox), b, FALSE, FALSE, 5);
+	g_signal_connect(G_OBJECT(b), "clicked", G_CALLBACK(closeWindow), win);
+	g_object_unref( model );
+	
 	gtk_widget_show_all(win);
 	return;
 }
@@ -204,8 +559,7 @@ int activate(void *internal)
 			fclose(fp);
 		else
 			extract_pas(pas_path);
-		updateDevices(1);
-		messageBox( "Not implemented yet" );
+		addDeviceDialog(NULL,NULL);
 	}
 	else
 	{
@@ -249,19 +603,7 @@ INTERNAL* create_submenu(PLUGIN_INFO * info, char * name,INTERNAL_type type)
 
 int getinfo(PLUGIN_INFO *info)
 {
-	char path[1024];
-	FILE *fp;
-
-	sprintf(path, "%s/.config-button/sound-output.conf", getenv("HOME"));
-	fp = fopen(path, "r");
-	if (fp)
-	{
-		fscanf(fp, "%s\n", pas_path);
-		fclose(fp);
-	}
-	else
-		sprintf(pas_path, "%s/pas", getenv("HOME"));
-	
+	set_pas_path();
 	if (info->submenu == NULL) //first run
 	{
 		info->label = malloc(128);
